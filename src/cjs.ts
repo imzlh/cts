99899ed24cae6c8e249e63339528af46f1cc7f1e
imzlh <2131601562@qq.com>
2026-06-25 20:55:45 +0800

    add sqlite/sqlite3 registration

--- src/cjs.ts
+++ src/cjs.ts
@@ -54,8 +54,8 @@ export const BUILTINS = new Set([
     'assert','buffer','child_process','cluster','console','constants',
     'crypto','dgram','dns','domain','events','fs','http','http2','https',
     'module','net','os','path','perf_hooks','process','punycode',
-    'querystring','readline','repl','stream','string_decoder',
-    'timers','tls','trace_events','tty','url','util','v8','vm',
+    'querystring','readline','repl','sqlite','stream','string_decoder',
+    'sqlite3','timers','tls','trace_events','tty','url','util','v8','vm',
     'worker_threads','zlib',
 ]);
 
